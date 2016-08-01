3ce9bb569c6fea714d9fe11a30cf424716116bdf
Howard Roark <howie@codingcoop.org>
2016-08-01 09:46:47 -0400

    Adding a TemplateView example.

---
+++ examples/templateview/script.js
@@ -0,0 +1,155 @@
+$(function () {
+	Backbone.history.start();
+	state.fetch();
+	todos.fetch();
+	main.render();
+});
+
+// Domain
+var State = Backbone.Model.extend({
+	localStorage: new Backbone.LocalStorage('state'),
+	defaults: {
+		filter: 'all'
+	}
+});
+
+var state = new State({id: 'todos'});
+
+var Todo = Backbone.Model.extend({
+	defaults: {
+		status: 'active',
+		isEditing: false
+	}
+});
+
+var Todos = Backbone.Collection.extend({
+	Model: Todo,
+	localStorage: new Backbone.LocalStorage('todos')
+});
+
+var todos = new Todos();
+
+var Router = Backbone.Router.extend({
+	routes: {
+		'*filter':'setFilter'
+	},
+	setFilter: function (filter) {
+		if (filter === null) {
+			filter = 'all';
+		}
+		state.save({
+			filter: filter
+		});
+	}
+});
+
+new Router();
+
+// UI
+var FormView = TemplateView.extend({
+	template: '#formView',
+	events: {
+		submit:'submit'
+	},
+	submit: function (e) {
+		var todo = new Todo({
+			status: 'active',
+			label: e.target[0].value
+		});
+		this.collection.add(todo);
+		todo.save();
+		return false;
+	}
+});
+
+var ItemView = TemplateView.extend({
+	template: '#itemView',
+	events: {
+		dblclick:'edit',
+		keydown: 'escape',
+		'submit .editForm':'submitForm',
+		'blur .edit':'blurInput',
+		'click .toggle':'toggle',
+		'click .destroy':'destroy'
+	},
+	edit: function (e) {
+		this.model.save({
+			isEditing: true
+		});
+		$(e.currentTarget).find('input').focus().select();
+	},
+	escape: function (e) {
+		if (e.which === 27) {
+			this.model.save({
+				isEditing: false
+			});
+		}
+	},
+	submitForm: function (e) {
+		this.update(e.target[0].value);
+		return false;
+	},
+	blurInput: function (e) {
+		this.update(e.target.value);
+		return false;
+	},
+	update: function (label) {
+		if (label === '') {
+			this.model.destroy();
+		} else {
+			this.model.save({
+				isEditing: false,
+				label: label
+			});
+		}
+	},
+	toggle: function (e) {
+		var status = 'active';
+		if (e.target.checked) {
+			status = 'completed';
+		}
+		this.model.save({
+			status: status
+		});
+	},
+	destroy: function (e) {
+		this.model.destroy();
+	}
+});
+
+var MainView = TemplateView.extend({
+	ChildView: ItemView,
+	SubViews: [FormView],
+	state: state,
+	collection: todos,
+	template: '#mainView',
+	templateContext: function () {
+		return {
+			allItems: this.collection.length,
+			activeItems: this.collection.where({ status: 'active' }).length
+		};
+	},
+	events: {
+		'click .toggle-all':'toggleAll',
+		'click .clear-completed':'clearCompleted'
+	},
+	toggleAll: function () {
+		var activeItems = this.collection.where({ status: 'active' });
+		var status = 'completed';
+		if (activeItems.length === 0) {
+			status = 'active';
+		}
+		this.collection.save({ status: status });
+	},
+	clearCompleted: function () {
+		this.collection.destroy({ status: 'completed' });
+		return false;
+	}
+});
+
+var main = new MainView();
+
+// Polyfill type stuff not directly related
+if ('ontouchstart' in document.documentElement === false) {
+	$('html').addClass('no-touch');
+}
